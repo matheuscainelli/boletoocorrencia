b2d26bcfe4af15c3fda0e4e9ccb643d05ed5aaff
williamsimionatto <62775299+williamsimionatto@users.noreply.github.com>
2020-05-05 15:25:53 -0300

    Create funcoes.php

---
+++ class/funcoes.php
@@ -0,0 +1,13 @@
+<?php
+function printr($var) {
+    echo '<pre>';
+    print_r($var);
+    echo '</pre>';
+}
+
+function vardump($var) {
+    echo '<pre>';
+    var_dump($var);
+    echo '</pre>';
+}
+?>
